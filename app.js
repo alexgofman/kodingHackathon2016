615d6fb4c32633ff4f042049346fa1b78aa274ca
Alex Gofman <gofmanalex13@gmail.com>
2016-02-20 23:04:33 +0400

    added states

--- app.js
+++ app.js
@@ -28,4 +28,19 @@ app.controller('MainCtrl', [
       post.upvotes += 1;
     };
 
-  }]);
+}]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+}]);
