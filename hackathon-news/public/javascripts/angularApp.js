0537f6a3b29770b497b62e8b2e2e2cdb5a124f36
Alex Gofman <gofmanalex13@gmail.com>
2016-02-21 12:58:16 +0400

    add upvoting posts function

--- hackathon-news/public/javascripts/angularApp.js
+++ hackathon-news/public/javascripts/angularApp.js
@@ -14,6 +14,12 @@ app.factory('posts', ['$http', function($http) {
       o.posts.push(data);
     });
   };
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+      .success(function(data) {
+        post.upvotes += 1;
+      });
+  };
 
   return o;
 }]);
@@ -35,7 +41,7 @@ app.controller('MainCtrl', [
     };
 
     $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+      posts.upvote(post);
     };
 
 }]);
